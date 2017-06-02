0e4c38dbce258e9e0bc7fcd7fba02154a50143c1
DV <github email address>
2017-06-02 16:14:28 +0900

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,48 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    
+    scraped_students = Array.new
+
+    index_page = Nokogiri::HTML(open(index_url))
+    student_elements = index_page.css("div.roster-cards-container div.student-card a")
+    student_elements.each{|student_element|
+      student_attributes = Hash.new
+      student_attributes[:name] =
+          student_element.css("div.card-text-container h4.student-name").text
+      student_attributes[:location] =
+          student_element.css("div.card-text-container p.student-location").text
+      student_attributes[:profile_url] = student_element.attribute("href").value
+      scraped_students.push(student_attributes)
+    }
+    return scraped_students
   end
+
+  # Social_icon_map maps social-symbol to corresponding src substring
+  Social_icon_map = {
+    twitter: "twitter", linkedin: "linkedin", github: "github", blog: "rss" }
 
   def self.scrape_profile_page(profile_url)
-    
+    scraped_student = Hash.new
+
+    student_page = Nokogiri::HTML(open(profile_url))
+    # link scraping
+    link_elements = student_page.css("div.social-icon-container a")
+    link_elements.each { |link_element|
+      Social_icon_map.each { |symbol, src_substring|
+        if link_element.css("img").attribute("src").value.include?(src_substring)
+          scraped_student[symbol] = link_element.attribute("href").value
+          break
+        end
+      }
+    }
+    # profile & bio scraping
+    scraped_student[:profile_quote] =
+        student_page.css("div.vitals-text-container div.profile-quote").text
+    scraped_student[:bio] =
+        student_page.css("div.bio-content div.description-holder p").text
+    return scraped_student
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,27 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
-
-  @@all = []
-
-  def initialize(student_hash)
-    
-  end
+  # Class methods and attributes
+  @@all = Array.new
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|student_hash| Student.new(student_hash)}
+  end
+
+  def self.all()
+    return @@all
+  end
+
+  # Instance methods and attributes
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog,
+                :profile_quote, :bio, :profile_url
+
+  def initialize(student_hash)
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each {|key, value| self.send(("#{key}="), value)}
   end
 
-  def self.all
-    
-  end
 end
-
